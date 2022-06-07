e1bd8fbd15aaaefe8e69c58e40b42f6ff98e5dab
thankGodItsWednesday <jshsarada@gmail.com>
2022-06-06 22:00:14 -0700

    Uncontrolled Form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Fade, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Fade, Nav, NavbarToggler, Collapse, NavItem,
+    Button, Modal, ModalHeader, ModalBody, FormGroup, Label, Input, Form } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -7,15 +8,33 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isNavOpen: true
+            isNavOpen: true,
+            isModalOpen: false
         };
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+
+    }
+
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+        event.preventDefault();
+
     }
 
     render() {
@@ -50,9 +69,18 @@ class Header extends Component {
                                             <NavLink className="nav-link" to="/contactus"><span className='fa fa-address-card fa-lg'></span> Contact Us
                                             </NavLink>
                                         </NavItem>
-                                    </Nav>
+                                    </Nav>                                    
                                 </Collapse>
                             </div>
+                            <div className='col-12 col-md-4'>
+                                <Nav navbar>
+                                    <NavItem>
+                                        <Button outline onClick={this.toggleModal} id="LoginButton">
+                                            <span className='fa fa-sign-in fa-lg'></span> Login
+                                         </Button>
+                                    </NavItem>
+                                </Nav>
+                            </div>                            
                         </div>
                     </div>
                 </Navbar>
@@ -66,6 +94,29 @@ class Header extends Component {
                     </div>
                 </div>
                 </Fade>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor='username'>Username</Label>
+                                <Input type='text' id="useraname" name='username' innerRef={(input) => this.username = input}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='password'>Password</Label>
+                                <Input type='password' id="password" name='password' innerRef={(input) => this.password = input}/>
+                            </FormGroup>
+                            <FormGroup  check>
+                                <Label check>
+                                    <Input type='checkbox' name='remember' innerRef={(input) => this.remember = input}/>
+                                    Remember Me
+                                </Label>
+                            </FormGroup>
+                            <Button type='submit' value="submit" color="primary">Login</Button>
+                        </Form>
+
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
